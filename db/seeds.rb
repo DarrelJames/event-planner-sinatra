861779461118f3e7293a6f681b15e8967721df54
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-20 10:29:05 -0700

    seed database

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+user1 = User.create(email: "user1@email.com", password: "password")
+user2 = User.create(email: "user2@email.com", password: "password")
+user3 = User.create(email: "user3@email.com", password: "password")
+
+event1 = user1.events.create(name: "User1 Event1", event_date: "10/10/10", start_time: "10", end_time: "12")
+event2 = user1.events.create(name: "User1 Event2", event_date: "10/10/10", start_time: "10", end_time: "12")
+event3 = user2.events.create(name: "User2 Event1", event_date: "10/10/10", start_time: "10", end_time: "12")
+event4 = user2.events.create(name: "User2 Event2", event_date: "10/10/10", start_time: "10", end_time: "12")
+event5 = user3.events.create(name: "User3 Event1", event_date: "10/10/10", start_time: "10", end_time: "12")
+event6 = user3.events.create(name: "User3 Event2", event_date: "10/10/10", start_time: "10", end_time: "12")
+
+event1.create_venue(name: "venue1", address: "san jose, ca")
+event2.create_venue(name: "venue1", address: "san jose, ca")
+event3.create_venue(name: "venue1", address: "san jose, ca")
+event4.create_venue(name: "venue1", address: "san jose, ca")
+event5.create_venue(name: "venue1", address: "san jose, ca")
+event6.create_venue(name: "venue1", address: "san jose, ca")
