a372a6d582f868a463495897a7d6fbd346a82e02
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-20 08:49:24 -0700

    Reverse change columng

--- db/migrate/20191020072316_change_event_columns.rb
+++ db/migrate/20191020072316_change_event_columns.rb
@@ -1,7 +1,12 @@
 class ChangeEventColumns < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column(:events, :event_date, :string)
     change_column(:events, :start_time, :string)
     change_column(:events, :end_time, :string)
+  end
+  def down
+    change_column(:events, :event_date, :datetime)
+    change_column(:events, :start_time, :datetime)
+    change_column(:events, :end_time, :datetime)
   end
 end
