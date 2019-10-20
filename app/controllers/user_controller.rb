298dd20de8aa30bb64ca4f47a6c074f51b7dc3a3
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-19 22:46:13 -0700

    Can create a new user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,22 @@ class UserController < ApplicationController
 
 
   get '/signup' do
-
     erb :"users/signup"
+  end
+
+  post '/signup' do
+    user = User.new(params)
+
+    if user.save
+      session[:user_id] = user.id
+      redirect to('/users')
+    else
+      redirect to('/signup')
+    end
+  end
+
+  get '/users' do
+
+    erb :"/users/index"
   end
 end
