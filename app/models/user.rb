917621683426dd1cfd9b02d61fa87c9edd40f380
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-19 21:42:45 -0700

    Validates email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :events
   has_many :guests, through: :events
+  validates :email, presence: true
+  validates_uniqueness_of :email
 end
