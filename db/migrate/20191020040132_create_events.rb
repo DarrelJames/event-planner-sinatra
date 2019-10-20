115f4d9a225b58537394c7fedd36607c9ada8ac9
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-20 08:53:09 -0700

    change data type to string

--- db/migrate/20191020040132_create_events.rb
+++ db/migrate/20191020040132_create_events.rb
@@ -2,9 +2,9 @@ class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
       t.string :name
-      t.datetime :event_date
-      t.datetime :start_time
-      t.datetime :end_time
+      t.string :event_date
+      t.string :start_time
+      t.string :end_time
       t.belongs_to :user
       t.belongs_to :venue
     end
