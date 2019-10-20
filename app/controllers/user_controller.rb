c5b09348f1c9b21c072ccae485b05df58ea4957d
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-19 23:53:42 -0700

    set session id

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -27,8 +27,9 @@ class UserController < ApplicationController
 
   post '/login' do
     user = User.find_by(email: params[:email])
-
+    
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect to('/users')
     else
       redirect to('/login')
