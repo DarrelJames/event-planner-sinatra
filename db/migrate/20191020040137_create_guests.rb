287e49b87b221c946396e1edf9aea872e5ff3688
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-20 19:00:21 -0700

    remove rsvp

--- db/migrate/20191020040137_create_guests.rb
+++ db/migrate/20191020040137_create_guests.rb
@@ -2,8 +2,8 @@ class CreateGuests < ActiveRecord::Migration[5.2]
   def change
     create_table :guests do |t|
       t.string :name
-      t.belongs_to :user      
-      t.boolean :rsvp
+      t.belongs_to :user
+      
 
     end
   end
