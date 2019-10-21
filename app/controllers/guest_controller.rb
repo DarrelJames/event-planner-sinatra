0e1f114f73ca8a64ef9e3764fbdf0a4bf32a2615
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-20 17:08:48 -0700

    Add guest forms and save

--- app/controllers/guest_controller.rb
+++ app/controllers/guest_controller.rb
@@ -1,3 +1,17 @@
 class GuestController < ApplicationController
 
+  get '/guests/new' do
+    erb :"guests/new"
+  end
+
+  get '/guests' do
+    erb :index
+  end
+
+  post '/guests' do
+    guest = current_user.guests.build(params[:guest])
+    if guest.save
+      redirect to("/guests")
+    end
+  end
 end
