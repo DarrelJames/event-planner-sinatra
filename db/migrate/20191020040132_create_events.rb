9ab077ca3ed1b69af3a7e9c5510c0592b9537f04
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-20 19:34:34 -0700

    remove rsvp

--- db/migrate/20191020040132_create_events.rb
+++ db/migrate/20191020040132_create_events.rb
@@ -4,8 +4,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :event_date
       t.string :start_time
-      t.string :end_time
-      t.boolean :rsvp
+      t.string :end_time      
       t.belongs_to :user
       t.belongs_to :venue
     end
