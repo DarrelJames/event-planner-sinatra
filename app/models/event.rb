460c7282023caec76d5aaaf9b84b80741f898947
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-20 17:48:49 -0700

    Slug module

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,18 @@
 class Event < ActiveRecord::Base
+  extend Slugify::ClassMethods
+  include Slugify::InstanceMethods
+
   belongs_to :user
   belongs_to :venue
   has_and_belongs_to_many :guests
 
   validates :name, presence: true
 
-  def slug
-    self.name.downcase.gsub(" ", "-")
-  end
-
-  def self.find_by_slug(slug)
-    self.all.find {|event| event.slug == slug}
-  end
+  # def slug
+  #   self.name.downcase.gsub(" ", "-")
+  # end
+  #
+  # def self.find_by_slug(slug)
+  #   self.all.find {|event| event.slug == slug}
+  # end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,7 @@
 class Guest < ActiveRecord::Base
+  extend Slugify::ClassMethods
+  include Slugify::InstanceMethods
+  
   belongs_to :user
   has_and_belongs_to_many :events
 end
