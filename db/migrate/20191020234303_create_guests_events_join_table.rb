2aef7588742a10bea90b96930d94c4fcd215a18c
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-22 16:45:21 -0700

    chhanged table to invitations and added attending column

--- db/migrate/20191020234303_create_guests_events_join_table.rb
+++ db/migrate/20191020234303_create_guests_events_join_table.rb
@@ -1,8 +1,9 @@
 class CreateGuestsEventsJoinTable < ActiveRecord::Migration[5.2]
   def change
-    create_table :events_guests, id: false do |t|
+    create_table :invitations, id: false do |t|
       t.belongs_to :event
       t.belongs_to :guest
+      t.boolean :attending
     end
   end
 end
