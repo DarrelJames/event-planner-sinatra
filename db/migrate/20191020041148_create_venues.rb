de7c13fc98c5e1707c612c845fcfe8d529d63ba4
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-21 22:59:33 -0700

    moved to create venues

--- db/migrate/20191020041148_create_venues.rb
+++ db/migrate/20191020041148_create_venues.rb
@@ -2,7 +2,11 @@ class CreateVenues < ActiveRecord::Migration[5.2]
   def change
     create_table :venues do |t|
       t.string :name
-      t.string :address      
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip_code 
+
     end
   end
 end
--- db/migrate/20191021230725_add_attributes_to_venues.rb
+++
@@ -1,7 +0,0 @@
-class AddAttributesToVenues < ActiveRecord::Migration[5.2]
-  def change
-    add_column :venues, :city, :string
-    add_column :venues, :state, :string
-    add_column :venues, :zip_code, :string
-  end
-end
