9fa31ec2ad75ff238e5ee766de9c8e3305eeb19c
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-22 22:19:02 -0700

    took out dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :events, dependent: :destroy
-  has_many :guests, dependent: :destroy
-  
+  has_many :events
+  has_many :guests
+
   validates :email, presence: true
   validates_uniqueness_of :email
 end
