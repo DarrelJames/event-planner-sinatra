2b6dc8d2ae606666f518ef9881086bd36177acb3
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-23 16:24:29 -0700

    Create rsvp forms

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -9,4 +9,10 @@ class WebsiteController < ApplicationController
     @event = Event.find_by_slug(params[:slug])
     erb :"venues/show", :layout => :website
   end
+
+  get '/events/:slug/rsvp' do
+    @event = Event.find_by_slug(params[:slug])
+    # @guest = current_user.guests.find_by(name: session[:guest_name])
+    erb :"/guests/rsvp"
+  end
 end
