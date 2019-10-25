ff4a93b2d49d6262acae02533b031390ae8935fd
Darrel Castellano <dcastellano2007@gmail.com>
2019-10-25 12:26:32 -0700

    Add route tfor rsvp forms

--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -13,6 +13,25 @@ class WebsiteController < ApplicationController
   get '/events/:slug/rsvp' do
     @event = Event.find_by_slug(params[:slug])
     # @guest = current_user.guests.find_by(name: session[:guest_name])
-    erb :"/guests/rsvp"
+    erb :"/guests/rsvp", :layout => :website
+  end
+
+  post '/events/:slug/rsvp/guest' do
+    event = Event.find_by_slug(params[:slug])
+    guest = event.guests.find_by(name: params)
+    if guest
+      session[:guest_id] = guest.id
+      redirect to("/events/#{event.slug}/rsvp")
+    else
+      flash[:message] = "Sorry, you're name is not on the guestlist. Please try again or Contact the host."
+      redirect to("/events/#{event.slug}/rsvp")
+    end
+
+  end
+
+  post '/events/:slug/rsvp' do
+    event = Event.find_by_slug(params[:slug])
+    guest = Guest.find_by(name: session[:guest_id])
+    rsvp = Rsvp.new
   end
 end
